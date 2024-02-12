48f1ec51aaacd64fb3b9dd43abdfa9b7d60b88ca
Cedric-Assemien <122783332+Cedric-Assemien@users.noreply.github.com>
2024-02-12 22:34:59 +0000

    server.py

--- server.py
+++ server.py
@@ -46,6 +46,11 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+    
+    if int(competition['numberOfPlaces']) < placesRequired  :
+        flash('Désolé, il n\'y a pas assez de places disponibles.')
+        return render_template('welcome.html', club=club, competitions=competitions)
+        
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
